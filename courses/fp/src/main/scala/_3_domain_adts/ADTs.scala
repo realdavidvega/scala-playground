b271fee3219b9e0ca23f0b594c5c7a9967d7dc63
David Vega Lichacz <7826728+realdavidvega@users.noreply.github.com>
2024-04-15 17:08:58 +0200

    feat: add domain + ADTs

---
+++ courses/fp/src/main/scala/_3_domain_adts/ADTs.scala
@@ -0,0 +1,129 @@
+package _3_domain_adts
+
+import java.time.LocalDate
+
+object ADTs:
+  @main
+  def main(): Unit =
+    // Algebraic Data Types
+    // The FP-way to model data
+    // Two ways to model data:
+    // - Putting values together (product)
+    // - Giving a choice (sum)
+    // Pattern matching to extract values
+
+    // Product types
+    // Immutable and cannot be extended
+    // Allow pattern matching
+    case class User(id: Int, name: String)
+    case class Account(id: Int, owner: User, balance: Int)
+    case class Debit(account: Account, amount: Int)
+
+    val user = User(1, "John")
+    println(user)
+    // User(1,John)
+
+    val debitOp = Debit(Account(1, user, 100), 50)
+    println(debitOp)
+    // Debit(Account(1,User(1,John),100),50)
+
+    // Sealed types
+    // Restricted hierarchy of classes
+    sealed abstract class Color
+    case object Red extends Color
+    case object Green extends Color
+    case object Blue extends Color
+
+    val color: Color = Red
+    println(color)
+    // Red
+
+    // Sum types
+    // Giving a choice, aka, coproduct types
+    case class Role(name: String)
+
+    sealed trait SpecialUser extends Product with Serializable
+    case class Admin(name: String, roles: List[Role]) extends SpecialUser
+    case class Customer(name: String, birthDate: LocalDate) extends SpecialUser
+
+    // Pattern matching
+    object UserMatch {
+      def name(user: SpecialUser): String = user match {
+        case Admin(name, _) => name
+        case Customer(name, _) => name
+      }
+    }
+
+    val customerName = UserMatch.name(Customer("John", LocalDate.now()))
+    println(customerName)
+    // John
+
+    // Exhaustive pattern matching
+    def nameWrong(user: SpecialUser): String = user match {
+      case Admin(name, _) => name
+    }
+    // match may not be exhaustive. It would fail on pattern case: Customer(_, _)
+
+    // Take into account, that, in Scala, every case class extends Product and Serializable automatically
+    // So every element in both does so too
+    // Scala includes it in the inferred type
+
+    // Either is a sum type
+    sealed trait MyEither[+E, +A]
+
+    case class Left[+E](value: E) extends MyEither[E, Nothing]
+    case class Right[+A](value: A) extends MyEither[Nothing, A]
+
+    // Cats brings many niceties to the table
+    import cats.syntax.all._
+
+    type Error = String
+    type Success = Int
+
+    def toInt(str: String): Either[Error, Success] =
+      if(str.forall(_.isDigit)) str.toInt.asRight
+      else s"Could not convert $str to int".asLeft
+
+    val error = toInt("0a")
+    println(error)
+    // Left(Could not convert 0a to int)
+
+    // Either as block for sum types
+    // Nested Either for arbitrary number of choices
+
+    type Choice = Either[Int, Either[Boolean, String]]
+
+    def typed(from: String): Choice =
+      if (from.forall(_.isDigit)) from.toInt.asLeft
+      else if (from == "true" || from == "false") from.toBoolean.asLeft.asRight
+      else from.asRight.asRight
+
+    val typedTrue = typed("true")
+    println(typedTrue)
+    // Right(Left(true))
+
+    // OOP vs FP
+    // OOP classes implementing interfaces
+    // - Mixes data with behavior
+    // - Uses fields and methods
+    // FP function modules transforming data
+    // - Decouples data from behavior
+    // - Uses data (ADTs) and functions
+    // Do not put methods into ADT classes
+
+    // What are those + before the type arguments? -> Variance annotations
+    sealed trait Either3[+E, +A]
+
+    // Interaction between types and subtyping
+    // Interaction between FP and OOP
+    // <: means "subtype of" in OOP
+    // Either[E, List[A]] <: Either[E, Seq[A]] <: Either[E, Any]
+
+    // In Scala 3
+    // There is direct syntax for ADTs
+    enum MyUser:
+      case Admin(name: String, roles: List[Role])
+      case Customer(name: String, birthDate: LocalDate)
+
+  end main
+end ADTs
---
+++ courses/fp/src/main/scala/_3_domain_adts/CatsDataTypes.scala
@@ -0,0 +1,79 @@
+package _3_domain_adts
+
+import cats.{Eval, Semigroup}
+import cats.data.NonEmptyList
+
+import java.time.LocalDate
+
+object CatsDataTypes:
+  @main
+  def main3(): Unit =
+    // NonEmptyList, which is a Semigroup
+    // They can be combined
+    // We also have ValidatedNel[E, A], which is an alias for Validated[E, NonEmptyList[A]]
+
+    // NonEmptyChain, which is a Semigroup
+    // Alternative to List, with constant time append and prepend
+    // We have NonEmptyChain too
+
+    // Complexity
+    //            Chain   List
+    // Prepend   O(1)     O(1)
+    // Append    O(1)     O(n)
+    // Traverse  O(n)     O(n2)
+
+    // Scala is eagerly evaluated
+    // Arguments are eagerly evaluated before the function is called
+    val y = List(1, 2, 3).map(x => x + 1)
+
+    // Scala has lazy to avoid eager evaluation
+    // Quite heavy since it uses @volatile
+    // def only evaluated when called
+    // by-name parameters only evaluated when used
+
+    // Eval, allows to control how pure expressions must be evaluated
+    // Eval.now -> compute once + cache the result
+    // Eval.later -> more efficient, lazy
+    // Eval.always -> evaluate every time
+    // Eval is stack-safe
+    val lazyEval = Eval.later {
+      println("Working...")
+      1 + 2 * 3
+    }
+
+    val do1 = lazyEval.value
+    // Working...
+    // 1 + 2 * 3
+
+    val do2 = lazyEval.value
+    // 1 + 2 * 3
+
+    // Option
+    import cats.syntax.all._
+
+    val empty: Option[Nothing] = None
+    // val empty: Option[Int] = None
+    val emptyInf: Option[Int] = none[Int]
+    // val emptyInf: Option[Int] = None
+
+    val maybeFive: Option[Int] = Some(5)
+    // val maybeFive: Option[Int] = Some(5)
+    val maybeFiveInf: Option[Int] = 5.some
+    // val maybeFiveInf: Option[Int] = Some(5)
+
+    // Either
+    val left: Either[String, Nothing] = Left("Error")
+    // val left: Either[String, Int] = Left("Error")
+    val leftInf: Either[String, Int] = "Error".asLeft
+
+    val right: Either[Nothing, Int] = Right(5)
+    // val right: Either[String, Int] = Right(5)
+    val rightInf: Either[String, Int] = 5.asRight
+
+    // Type safe replacements
+    // Using === or !==
+    // Order is the type-safe replacement for compareTo
+    // Show is the type-safe replacement for toString
+
+  end main3
+end CatsDataTypes
